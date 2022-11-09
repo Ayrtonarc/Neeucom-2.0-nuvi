f96c8db4f201ad25499fa669ccb2e3683ec36f7e
Ayrton S <ayrton.arc@gmail.com>
2022-11-08 22:10:02 -0800

    configuracion del servidor

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 //importar dependencias
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
 //
 console.log("API node Neeucom");
 //Conexion a la base de datos
 connection();
 //Crear servidor de node
-
+const app = express();
+const puerto = 3900;
 //Configurar cors
-
+app.use(cors());
 //Convertir los datos  del body a objetos
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Cargar configuracion de rutas
 
+//ruta de prueba
+app.get("/ruta-prueba", (req, res) => {
+    return res.status(200).json(
+    {
+        "id": 1, 
+        "nombre": "Ayrton",
+        "web" : "neeucom.com"
+
+    })
+});
+
 //Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("servidor ejecutandose en el puerto: ", 3900)
+});
