6f52e0466f85dc9c552810380e59a24dab0be759
Ayrton S <ayrton.arc@gmail.com>
2022-11-09 14:24:18 -0800

    Rutas y controladores de prueba

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+//Actions prueba
+const pruebaUser = (req, res) => {
+        return res.status(200).send({
+            message: "Mensaje enviado desde: controllers/user.js"
+        })
+}
+//Exportar  acciones
+module.exports = {
+    pruebaUser
+}
