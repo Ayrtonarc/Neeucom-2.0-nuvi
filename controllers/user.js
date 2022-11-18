0515088d8a4e56233c1327468846f30fa0e13be1
Ayrton S <ayrton.arc@gmail.com>
2022-11-17 20:28:23 -0800

    guardando usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 
+const { memoryStorage } = require("multer");
+const User = require("../models/user");
+
 //Actions prueba
 const pruebaUser = (req, res) => {
         return res.status(200).send({
@@ -7,9 +10,22 @@ const pruebaUser = (req, res) => {
 }
 
 const register = (req, res) => {
+    let params = req.body;
+
+    if(!params.name || !params.lastname || !params.username || !params.email || !params.password ){
+        return res.status(400).json({
+            status: 'error',
+            message: 'Faltan datos por enviar'
+        });
+    }
+    let usersaver = new User(params);
+
     return res.status(200).json({
-        message: "Accion de registro de usuarios"
+        message: "Accion de registro de usuarios",
+        params,
+        usersaver
     })
+    
 }
 //Exportar  acciones
 module.exports = {
